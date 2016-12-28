6b47a82b1257410df3d3a94ca85bbaa3737fec3f
pasha <p.korenev@voroninstudio.eu>
2016-12-28 15:02:10 +0200

    app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     @slider_images = @page_instance.slider_images
     @testimonials = Testimonial.published.sort_by_sorting_position
     @speakers = Speaker.published
+    @club_companies = HomeClubCompany.published.sort_by_sorting_position
   end
 
   def about_us
