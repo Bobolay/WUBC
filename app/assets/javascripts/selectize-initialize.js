f535b257776d1613ccec2383e2ef4186454d99cd
Bobolay <bogdan4uk@gmail.com>
2017-01-10 15:49:25 +0200

    fixed members filter

--- app/assets/javascripts/selectize-initialize.js
+++ app/assets/javascripts/selectize-initialize.js
@@ -1,4 +1,6 @@
 $('#selectize-field-1, #selectize-field-2, #selectize-field-3, #selectize-field-4').selectize({
-    // allowEmptyOption: true,
-    sortField: 'text'
+    // sortField: 'text',
+    persist: false,
+    // createOnBlur: true,
+    // create: true
   })
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,7 @@ class MembersController < ApplicationController
   before_action :set_user, only: [:show]
   def index
     @members = Member.confirmed.approved.not_speakers.order("approved_at desc")
-    @names = @members.map{|m| {id: m.id, name_or_email: m.full_name_or_email} }
+    @names = @members.map{|m| {id: m.id, name_or_email: m.full_name_or_email(false)} }
     @companies = @members.map{|m| m.valid_companies }.flatten
     @industries = @companies.map{|c| {id: c.industry_id, name: c.industry_name } }.select(&:present?).uniq
     @regions = @companies.map(&:region).select(&:present?).uniq
