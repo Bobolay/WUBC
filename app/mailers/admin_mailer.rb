bf40d2ce8550680989eda3c6c3c03a3f5e93a727
pasha <p.korenev@voroninstudio.eu>
2017-01-10 16:06:42 +0200

    mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -9,20 +9,20 @@ class AdminMailer < ApplicationMailer
     @admin_root = ENV["#{Rails.env}.host"] + "/admin"
     @user = user
     @email_title = "Новий користувач чекає на підтвердження"
-    mail to: receivers("new_user_waiting_approval"), subject: @email_title
+    mail to: receivers("new_user_waiting_approval"), subject: @email_title, layout: "mailer"
   end
 
   def user_subscribed_to_event(member, event)
     @member = member
     @event = event
     @email_title = "Член клубу підписався на подію"
-    mail to: receivers("user_subscribed_to_event"), subject: @email_title
+    mail to: receivers("user_subscribed_to_event"), subject: @email_title, layout: "mailer"
   end
 
   def user_unsubscribed_from_event(member, event)
     @member = member
     @event = event
     @email_title = "Член клубу відписався від події"
-    mail to: receivers("user_unsubscribed_from_event"), subject: @email_title
+    mail to: receivers("user_unsubscribed_from_event"), subject: @email_title, layout: "mailer"
   end
 end
