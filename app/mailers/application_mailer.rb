e7053e50195a3a5fd2a87fc66a67ab31c12a483b
pasha <p.korenev@voroninstudio.eu>
2017-01-13 15:52:26 +0200

    app

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: "support@wubc.club"
   layout 'mailer'
   helper Cms::Helpers::UrlHelper
+  helper ApplicationHelper
 
   def receivers(name)
     config_class = "FormConfigs::#{name.classify}".constantize
