6a2896d50997046ee05cd18867755ceb5240fa8b
pasha <p.korenev@voroninstudio.eu>
2017-01-26 14:01:09 +0200

    cabinet and registration style

--- app/assets/javascripts/appear-initialize.js
+++ app/assets/javascripts/appear-initialize.js
@@ -7,6 +7,7 @@ if(is_touch_screen || width < 640) {
 	$document.on("ready page:load", function() {
 	  var $appeared = $('.about-us .text, .about-us .text-middle, .our-kitchen .text, .dish-frame, .inner-text-right, .line-left, .chef-photo, .price-right, .price-left, .left-to-right, .right-to-left');
 	  $appeared.appear();
+	  $appeared.filter(":appeared").trigger("appear")
 	  $(document.body).on('appear', function(e, $affected) {
 	    $affected.addClass('appeared')
 	  });
