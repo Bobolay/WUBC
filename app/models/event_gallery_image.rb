ab0cd59e1b55b019d06710b48f19a8e805e3b511
pasha <p.korenev@voroninstudio.eu>
2017-02-03 14:40:07 +0200

    app

--- app/models/event_gallery_image.rb
+++ app/models/event_gallery_image.rb
@@ -1,4 +1,8 @@
 class EventGalleryImage < Attachable::Asset
+  default_scope do
+    order("id asc")
+  end
+
   has_attached_file :data, styles: proc {|attachment| attachment.instance.attachment_styles },
                     url: "/system/attachable/assets/data/:id_partition/:style/:filename",
                     path: ":rails_root/public:url",
