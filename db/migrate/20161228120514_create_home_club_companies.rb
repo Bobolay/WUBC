8bc88e5cd3e8b8b69d80876cd062007b1a90515b
pkorenev <p.korenev@voroninstudio.eu>
2017-12-27 21:44:51 +0200

    migrations

--- db/migrate/20161228120514_create_home_club_companies.rb
+++ db/migrate/20161228120514_create_home_club_companies.rb
@@ -1,6 +1,6 @@
 class CreateHomeClubCompanies < ActiveRecord::Migration
   def up
-    create_table :home_club_companies do |t|
+    create_table :club_companies do |t|
       t.boolean :published
       t.integer :sorting_position
       t.string :name
@@ -10,12 +10,12 @@ class CreateHomeClubCompanies < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    HomeClubCompany.create_translation_table(:name, :description)
+    ClubCompany.create_translation_table(:name, :description)
   end
 
   def down
-    HomeClubCompany.drop_translation_table!
+    ClubCompany.drop_translation_table!
 
-    drop_table :home_club_companies
+    drop_table :club_companies
   end
 end
--- db/migrate/20170203114422_rename_home_club_company_to_club_company.rb
+++ db/migrate/20170203114422_rename_home_club_company_to_club_company.rb
@@ -1,6 +1,6 @@
 class RenameHomeClubCompanyToClubCompany < ActiveRecord::Migration
   def change
-    rename_table :home_club_companies, :club_companies
-    rename_table :home_club_company_translations, :club_company_translations
+    #rename_table :home_club_companies, :club_companies
+    #rename_table :home_club_company_translations, :club_company_translations
   end
 end
